589e94d7c319630f9c5108940ba10a360334a258
dec9ue <decque@gmail.com>
2025-03-16 14:29:22 +0900

    augmented termination proof of quick_sort

--- LeanStudy/QuickSort.lean
+++ LeanStudy/QuickSort.lean
@@ -1,30 +1,25 @@
 import Lean
 import Mathlib.Tactic.Ring
 
-def my_head {α : Type} (xs : List α) (h : xs ≠ []) : α :=
-  match xs with
-  | [] => absurd rfl h
-  | x :: _ => x
-
-def my_tail {α : Type} (xs : List α) (h : xs ≠ []) : List α :=
-  match xs with
-  | [] => absurd rfl h
-  | _ :: tl => tl
-
-theorem length_partition :
-  ∀ {α : Type} (p : α → Prop) [DecidablePred p] (xs : List α),
-  xs.length = (xs.partition p).1.length + (xs.partition p).2.length := by
+theorem filter_length : ∀ {α : Type} (p : α → Prop) [DecidablePred p] (xs : List α),
+  (xs.filter p).length ≤ xs.length := by
     intros α p h xs
     induction xs with
     | nil => rfl
     | cons x xs ih =>
-      simp [List.partition]
+      simp only [List.filter, List.length_cons]
       by_cases hpx : p x
-      sorry
-      sorry
+      .
+        simp only [hpx, decide_true, List.length_cons, add_le_add_iff_right]
+        exact ih
+      .
+        simp only [hpx, decide_false]
+        calc
+          _ ≤ xs.length := by simp only [ih]
+          _ ≤ xs.length + 1 := by apply Nat.le_add_right
 
 def my_quick_sort {α : Type} [LT α][DecidableLT α] : List α → List α :=
-  λ l => match l with
+  λ l => match hl : l with
   | [] => []
   | [x] => [x]
   | pivot :: xs =>
@@ -34,14 +29,29 @@ def my_quick_sort {α : Type} [LT α][DecidableLT α] : List α → List α :=
     let larger := parted.2
     have p_decidable : DecidablePred p := by
       infer_instance
-    have l_ : l = pivot :: xs := by sorry
-    have : l.length = smaller.length + 1 + larger.length := by
-      -- rewrite l to pivot :: xs
-      rw[l_]
-      rw[List.length_cons]
-      rw [length_partition p xs]
-      dsimp[smaller, larger,parted]
-      ring_nf
-      congr <;> ext x <;> simp
-
+    have : smaller.length < l.length := by
+      simp only [parted, larger, smaller]
+      simp only [List.partition_eq_filter_filter]
+      rw [hl]
+      rw [List.length_cons]
+      calc
+        _ ≤ xs.length := by
+          sorry
+        _ < xs.length + 1 := by
+          exact lt_add_one xs.length
+    have : larger.length < l.length := by
+      simp only [parted, larger, smaller]
+      simp only [List.partition_eq_filter_filter]
+      rw [hl]
+      rw [List.length_cons]
+      calc
+        _ ≤ xs.length := by
+          sorry
+        _ < xs.length + 1 := by
+          exact lt_add_one xs.length
+    have : (List.filter (fun b ↦ decide (b < pivot)) xs).length < xs.length + 1 := by
+      calc
+        _ ≤ xs.length := by apply filter_length
+        _ < xs.length + 1 := by exact lt_add_one xs.length
     my_quick_sort smaller ++ [pivot] ++ my_quick_sort larger
+termination_by x1 => x1.length
